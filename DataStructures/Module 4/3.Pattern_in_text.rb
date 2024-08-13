a327dc0075cc0d4b8f75a558444f0276b2b93faf
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-08-13 12:41:35 +0300

    Fixed time limit error of Problem 3

--- DataStructures/Module 4/3.Pattern_in_text.rb
+++ DataStructures/Module 4/3.Pattern_in_text.rb
@@ -5,23 +5,16 @@ P = 1_000_000_007
 
 def poly_hash(s)
   hash = 0
-  (s.length - 1).downto(0) { |i| hash = (hash * X + s[i].ord) % P } # Needs to be in reverse order
+  (s.length - 1).downto(0) { |i| hash = (hash * X + s[i].ord) % P }
   hash
 end
-
-# puts poly_hash('Test') # 535097971
 
 def precompute_hashes(text, length)
   hashes = Array.new(text.length - length + 1)
   hashes[text.length - length] = poly_hash(text[text.length - length..])
-  # puts "hashes[#{text.length - length}]: #{hashes[text.length - length]}"
-  # puts "Substring: #{text[text.length - length..]}"
+  x_powered = (X**length) % P ## takes O(log(length)) time :O, shouldn't be in the loop
   (text.length - length - 1).downto(0) do |i|
-    hashes[i] = (X * hashes[i + 1] + text[i].ord - text[i + length].ord * X**length) % P
-    # puts hashes[i] == poly_hash(text[i, length]) #### ???? #### found problem, poly hash needs to be in reverse order
-    # hashes[i] = poly_hash(text[i, length])
-    # puts "hashes[#{i}]: #{hashes[i]}"
-    # puts "Substring: #{text[i, length]}"
+    hashes[i] = (X * hashes[i + 1] + text[i].ord - text[i + length].ord * x_powered) % P
   end
   hashes
 end
@@ -40,4 +33,4 @@ pattern = gets.chomp
 text = gets.chomp
 puts pattern_in_text(pattern, text).join(' ')
 
-# time limit exceeded :(
+# time limit exceeded [ :(  ] #############################################################################
