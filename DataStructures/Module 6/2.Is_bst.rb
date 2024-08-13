f535080f91f9e12abeeb0b644ad4c6737a575d22
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-08-13 17:43:49 +0300

    Completed is_bst to solve problems 2 and 3, in O(N.Height)

--- DataStructures/Module 6/2.Is_bst.rb
+++ DataStructures/Module 6/2.Is_bst.rb
@@ -6,15 +6,24 @@ n.times do |i|
   tree[i] = gets.split.map(&:to_i)
 end
 
-def is_bst?(tree, i, min, max)
-  return true if tree.empty? || i == -1
-
-  return false if tree[i][0] < min || tree[i][0] > max
-
-  left = is_bst?(tree, tree[i][1], min, tree[i][0] - 1)
-  right = is_bst?(tree, tree[i][2], tree[i][0], max)
-
-  left && right
+def is_bst?(tree)
+  # node[0]: value
+  # node[1]: left index in tree
+  # node[2]: right index in tree
+  tree.each do |node|
+    max_left = node[1]
+    min_right = node[2]
+    max_left = tree[max_left][2] while max_left != -1 && tree[max_left][2] != -1
+    min_right = tree[min_right][1] while min_right != -1 && tree[min_right][1] != -1
+    if (max_left != -1 && tree[max_left][0] >= node[0]) || (min_right != -1 && tree[min_right][0] < node[0])
+      return false
+    end
+  end
+  true
 end
 
-puts is_bst?(tree, 0, -Float::INFINITY, Float::INFINITY) ? 'CORRECT' : 'INCORRECT'
+# O(n.height)
+
+# tree = [[4, 1, -1], [2, 2, 3], [1, -1, -1], [5, -1, -1]]
+
+puts is_bst?(tree) ? 'CORRECT' : 'INCORRECT'
