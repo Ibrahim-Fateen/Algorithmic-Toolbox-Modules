2397d2d5f3c1003e28679667db8ca8f34df9b878
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-08-14 20:24:03 +0300

    Foundation for Rope strings

--- DataStructures/Module 6/4.Set_range_sum.rb
+++ DataStructures/Module 6/4.Set_range_sum.rb
@@ -13,64 +13,120 @@ class Node
   end
 
   def update_height_and_sum
+    ########## needs work ???
     @height = [left&.height.to_i, right&.height.to_i].max + 1
     @sum = @key + left&.sum.to_i + right&.sum.to_i
     @parent&.update_height_and_sum
   end
 
-  # def sum(l, r)
-  #   sum = 0
-  #   node = find(l)
-  #   while node.key <= r
-  #     sum += node.key if node.key >= l
-  #     node = node.next
-  #   end
-  #   sum
-  # end
-
-  def sum_range(l, r)
-    common_root = first_common_root(l, r)
-    exclude = 0
-
-    left_node = common_root.find(l)
-    right_node = common_root.find(r)
-    while left_node != common_root
-      exclude += left_node.parent.sum - left_node.sum unless left_node.parent.left == left_node
-      left_node = left_node.parent
-    end
-
-    while right_node != common_root
-      exclude += right_node.parent.sum - right_node.sum unless right_node.parent.right == right_node
-      right_node = right_node.parent
-    end
-    common_root.sum - exclude
-  end
-
-  def add(key)
-    parent = find(key)
-    if key < parent.key
-      parent.left = Node.new(key)
-      parent.left.parent = parent
-    elsif key > parent.key
-      parent.right = Node.new(key)
-      parent.right.parent = parent
-    end
-    # rotate here if necessary??
-    parent.update_height_and_sum
-  end
-
   def find(key)
-    return self if key == @key
+    if key == @key
+      splay
+      return self
+    end
 
     if key < @key
-      return self if @left.nil?
+      if @left.nil?
+        splay
+        return self
+      end
 
       @left&.find(key)
     else
-      return self if @right.nil?
+      if @right.nil?
+        splay
+        return self
+      end
 
       @right&.find(key)
     end
+  end
+
+  def splay
+    while @parent
+      if @parent.parent.nil?
+        zig
+      elsif @parent.left == self && @parent.parent.left == @parent
+        zig_zig
+      elsif @parent.right == self && @parent.parent.right == @parent
+        zig_zig
+      else
+        zig_zag
+      end
+      update_height_and_sum
+    end
+  end
+
+  def zig
+    if @parent.left == self
+      @parent.left = @right
+      @right = @parent
+    else
+      @parent.right = @left
+      @left = @parent
+    end
+    @parent.update_height_and_sum
+    @parent.parent = self
+    @parent = nil
+  end
+
+  def zig_zig
+    grandparent = @parent.parent
+    if grandparent.left == @parent
+      b = @right
+      c = @parent.right
+      d = grandparent.right
+      @right = @parent
+      @parent.left = b
+      @parent.right = grandparent
+      grandparent.left = c
+      grandparent.right = d
+    else
+      b = @left
+      c = @parent.left
+      d = grandparent.left
+      @left = @parent
+      @parent.left = grandparent
+      @parent.right = b
+      grandparent.left = d
+      grandparent.right = c
+    end
+    @parent.parent = self
+    @parent = grandparent.parent
+    grandparent.parent = @parent
+    if @parent&.left == grandparent
+      @parent.left = self
+    else
+      @parent.right = self
+    end
+    grandparent.update_height_and_sum
+  end
+
+  def zig_zag
+    grandparent = @parent.parent
+    c = @left
+    d = @right
+    if grandparent.left == @parent
+      @left = @parent
+      @right = grandparent
+      grandparent.left = d
+      @parent.right = c
+    else
+      @left = grandparent
+      @right = @parent
+      grandparent.right = c
+      @parent.left = d
+    end
+    @parent.parent = self
+    grandparent.parent = self
+    @parent = grandparent.parent
+    if @parent&.left == grandparent
+      @parent.left = self
+    else
+      @parent.right = self
+    end
+    @left.update_height_and_sum
+    @right.update_height_and_sum
   end
 
   def delete_from_tree(key)
@@ -81,31 +137,31 @@ class Node
   end
 
   def delete
-    if @left.nil? && @right.nil?
-      if @parent.nil?
-        @key = nil
-        return
-      end
-      @parent.left = nil if @parent&.left == self
-      @parent.right = nil if @parent&.right == self
-    elsif @left.nil?
-      @parent.left = @right if @parent&.left == self
-      @parent.right = @right if @parent&.right == self
-      @right.parent = @parent
-    elsif @right.nil?
-      @parent.left = @left if @parent&.left == self
-      @parent.right = @left if @parent&.right == self
-      @left.parent = @parent
+    to_swap = if @left.nil?
+                successor_right
+              else
+                successor_left
+              end
+    @key = to_swap.key
+    to_swap.remove_from_tree
+    update_height_and_sum
+  end
+
+  def remove_from_tree
+    if @parent&.left == self
+      @parent.left = @left || @right
     else
-      to_swap = successor
-      @key = to_swap.key
-      to_swap.delete
+      @parent.right = @left || @right
     end
     @parent&.update_height_and_sum
   end
 
-  def successor
+  def successor_left
     @left&.max
+  end
+
+  def successor_right
+    @right&.min
   end
 
   def max
@@ -127,42 +183,112 @@ class Node
     node = node.parent while node.parent && node.parent.right == node
     node.parent
   end
+end
 
-  def first_common_root(left, right)
-    root = self
-    while root
-      if left < root.key && right < root.key
-        root = root.left
-      elsif left > root.key && right > root.key
-        root = root.right
-      else
-        return root
+class BST
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  def add(key)
+    if @root.nil?
+      @root = Node.new(key)
+    else
+      node = @root.find(key)
+      if node.key != key
+        new_node = Node.new(key)
+        new_node.parent = node
+        if key < node.key
+          node.left = new_node
+        else
+          node.right = new_node
+        end
       end
+      @root = node
+      @root.update_height_and_sum
     end
+  end
+
+  def find(key)
+    node = @root&.find(key)
+    @root = node if node
+    node
+  end
+
+  def delete(key)
+    to_delete = find(key)
+    return if to_delete.nil? || to_delete.key != key
+
+    if to_delete.left.nil? && to_delete.right.nil?
+      @root = nil
+    else
+      to_delete.delete
+      @root = to_delete
+    end
+  end
+
+  def sum_range(l, r)
+    left_node = find(l)
+    left_left_tree, left_right_tree = cut_left
+    right_node = left_right_tree.find(r)
+    right_left_tree, right_right_tree = left_right_tree.cut_right
+    sum = right_left_tree.root.sum
+    sum -= left_node.key if left_node.key > l
+    sum -= right_node.key if right_node.key < r
+    @root = merge(left_left_tree, merge(left_right_tree, right_right_tree)).root
+    if left_node == right_node
+      sum = left_node.key.between?(l, r) ? left_node.key : 0
+    end
+    sum
+  end
+
+  def cut_left
+    @root&.left&.parent = nil
+    left_root = @root&.left
+    @root&.left = nil
+    left_bst = BST.new
+    left_bst.root = left_root
+    [left_bst, self]
+  end
+
+  def cut_right
+    @root&.right&.parent = nil
+    right_root = @root&.right
+    @root&.right = nil
+    right_bst = BST.new
+    right_bst.root = right_root
+    [self, right_bst]
+  end
+
+  def merge(left_bst, right_bst)
+    if left_bst&.root.nil?
+      right_bst
+    elsif right_bst&.root.nil?
+      left_bst
+    else
+      right_bst.root.find(-Float::INFINITY)
+      right_bst.root.left = left_bst.root
+      right_bst.root.update_height_and_sum
+      right_bst
+  end
   end
 end
 
-bst_root = nil
+bst = BST.new
 M = 1_000_000_001
 x = 0
 add = lambda do |a|
-  if bst_root.nil? || bst_root.key.nil?
-    bst_root = Node.new((a + x) % M)
-  else
-    bst_root.add((a + x) % M)
-  end
+  bst.add((a + x) % M)
 end
 
 delete = lambda do |a|
-  bst_root&.delete_from_tree((a + x) % M)
+  bst.delete((a + x) % M)
 end
 
 find = lambda do |a|
-  if bst_root.nil? || bst_root.key.nil?
-    puts 'Not found'
-    return
-  end
-  node = bst_root&.find((a + x) % M)
+  node = bst.find((a + x) % M)
   if node && node.key == (a + x) % M
     puts 'Found'
   else
@@ -171,9 +297,9 @@ find = lambda do |a|
 end
 
 sum = lambda do |l, r|
-  x = bst_root ? bst_root.sum_range((l + x) % M, (r + x) % M) : x
+  x = bst.root ? bst.sum_range((l + x) % M, (r + x) % M) : x
   puts x
-end
+end # test thissssss, and test x
 
 n = gets.to_i
 n.times do
@@ -189,4 +315,3 @@ n.times do
     sum.call(query[1].to_i, query[2].to_i)
   end
 end
-
--- DataStructures/Module 6/5.Rope_string.rb
+++ DataStructures/Module 6/5.Rope_string.rb
@@ -1,2 +1,217 @@
 # frozen_string_literal: true
 
+class Node
+  attr_accessor :key, :index, :left, :right, :parent
+
+  def initialize(key, index)
+    @key = key
+    @index = index
+    @left = nil
+    @right = nil
+    @parent = nil
+  end
+
+  def update_index
+    # Call when merging???
+    @index = successor.index + 1
+    next_&.update_index
+  end
+
+  def successor
+    if @left
+      @left.max
+    else
+      parent = self
+      parent = parent.parent while parent.parent && parent.parent.right == parent
+      parent
+    end
+  end
+
+  def next_
+    if @right
+      @right.min
+    else
+      parent = self
+      parent = parent.parent while parent.parent && parent.parent.left == parent
+      parent
+    end
+  end
+
+  def max
+    current = self
+    current = current.right while current.right
+    current
+  end
+
+  def find(index)
+    if index == @index
+      self
+    elsif index < @index
+      @left.find(index)
+    else
+      @right.find(index)
+    end
+  end
+
+  def splay
+    while @parent
+      if @parent.parent.nil?
+        zig
+      elsif @parent.left == self && @parent.parent.left == @parent
+        zig_zig
+      elsif @parent.right == self && @parent.parent.right == @parent
+        zig_zig
+      else
+        zig_zag
+      end
+    end
+  end
+
+  def zig
+    if @parent.left == self
+      @parent.left = @right
+      @right = @parent
+      @right.parent = self
+    elsif @parent.right == self
+      @parent.right = @left
+      @left = @parent
+      @left.parent = self
+    end
+    @parent = nil
+  end
+
+  def zig_zig
+    grandparent = @parent.parent
+    if grandparent.left == @parent
+      b = @right
+      c = @parent.right
+      grandparent.left = c
+      @parent.left = b
+      @parent.right = grandparent
+      @parent.parent = self
+      @parent, grandparent.parent = grandparent.parent, @parent
+      @parent&.left == grandparent ? @parent&.left = self : @parent&.right = self
+      @right = grandparent.parent
+    else
+      b = @left
+      c = @parent.left
+      grandparent.right = c
+      @parent.right = b
+      @parent.left = grandparent
+      @parent.parent = self
+      @parent, grandparent.parent = grandparent.parent, @parent
+      @parent&.left == grandparent ? @parent&.left = self : @parent&.right = self
+      @left = grandparent.parent
+    end
+  end
+
+  def zig_zag
+    grandparent = @parent.parent
+    if grandparent.left == @parent
+      a = @left
+      b = @right
+      @left = @parent
+      @right = grandparent
+      @parent = grandparent.parent
+      grandparent.parent = self
+      @left.parent = self
+      @left.right = a
+      grandparent.left = b
+      @parent&.left == grandparent ? @parent.left = self : @parent.right = self
+    else
+      a = @left
+      b = @right
+      @left = grandparent
+      @right = @parent
+      @parent = grandparent.parent
+      grandparent.parent = self
+      @right.parent = self
+      @right.left = b
+      grandparent.right = a
+      @parent&.left == grandparent ? @parent.left = self : @parent.right = self
+    end
+  end
+end
+
+class SplayTree
+  attr_accessor :root
+  def initialize
+    @root = nil
+  end
+
+  def insert(node)
+    if @root.nil?
+      @root = node
+    else
+      current = @root
+      current = current.right while current.right
+      current.right = node
+      node.parent = current
+    end
+  end
+
+  def splay(index)
+    return if @root.nil?
+
+    current = @root.find(index)
+    current.splay
+    @root = current
+  end
+
+  def cut_left
+    
+  end
+
+  def cut_right
+
+  end
+
+  def merge(left, right)
+
+  end
+
+  def swap(i, j, k)
+    splay(i)
+    left_i, right_i = cut_left
+    right_i.splay(j)
+    left_j, right_j = cut_right
+    tree = merge(left_i, right_j)
+    tree.splay(k)
+    @root = merge(tree, left_j)&.root
+  end
+
+  def to_s
+    # in order traversal
+    result = []
+    stack = []
+    current = @root
+    while current || !stack.empty?
+      while current
+        stack << current
+        current = current.left
+      end
+      current = stack.pop
+      result << current.key
+      current = current.right
+    end
+    result.join
+  end
+end
+
+string = gets.chomp
+n = gets.chomp.to_i
+commands = []
+n.times do
+  commands << gets.chomp.split.map(&:to_i)
+end
+
+puts word_after_commands(string, commands)
+
+def word_after_commands(string, commands)
+  tree = SplayTree.new
+  string.each_char.with_index { |char, index| tree.insert(Node.new(char, index)) }
+  commands.each do |i, j, k|
+    tree.swap(i, j, k)
+  end
+  tree
+end
