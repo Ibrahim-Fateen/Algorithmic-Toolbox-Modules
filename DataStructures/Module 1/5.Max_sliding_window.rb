ff4f9fd9618d7cd1c267771c9563e00cc210b8ea
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-08-19 18:52:12 +0300

    Stacks and Queues Done :)  :*

--- DataStructures/Module 1/5.Max_sliding_window.rb
+++ DataStructures/Module 1/5.Max_sliding_window.rb
@@ -4,3 +4,16 @@ gets
 nums = gets.split.map(&:to_i)
 window_size = gets.to_i
 
+def solution(nums, window_size)
+  maxes = []
+  deque = []
+  nums.each_with_index do |num, i|
+    deque.shift while !deque.empty? && deque.first < i - window_size + 1
+    deque.pop while !deque.empty? && nums[deque.last] < num
+    deque << i
+    maxes << nums[deque.first] if i >= window_size - 1
+  end
+  maxes.join(' ')
+end
+
+puts solution(nums, window_size)
